1d3cc1e8c3343848a4336c2f40df40f1ec493f4d
Liana Bertoni <liana.bertoni@gmail.com>
2017-01-25 16:03:57 +0100

    fixed some error

--- src/grasp_quality_.cpp
+++ src/grasp_quality_.cpp
@@ -72,6 +72,8 @@ Contact GitHub API Training Shop Blog About
 #include <ctime>
 #include <time.h>
 
+#include "pseudo_inverse.h"
+
 
 
 
@@ -85,10 +87,10 @@ int n_cols = 0;
 
 
 int dim_file_row = 108;
-int dim_file_col = 10; 
+int dim_file_col = 86; 
 
 
-int n_c = 19; // numero di punti di contatto avendo supposto di avere 
+int n_c_max = 19; // numero di punti di contatto avendo supposto di avere 
 			  // un punto di contatto per ogni falange 
 			  // 3 falangi per il pollice
 			  // 4 per le altre dita  
@@ -97,17 +99,18 @@ int n_c = 19; // numero di punti di contatto avendo supposto di avere
 
 int quality_index = 0;
 
-
-
 std::vector<double> Quality;
+
+
 
 
 
 int main (int argc, char **argv)
 {
 
-	ros::init(argc, argv, "Soft_Hand_Jacobian");	// ROS node
+	ros::init(argc, argv, "Hand_Jacobian");	// ROS node
 	ros::NodeHandle nh;
+
 
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -125,7 +128,7 @@ std::string file_name;
 if(argc > 1)
 	file_name = std::string( argv[1] );
 else
-	file_name = std::string("box_db.csv");
+	file_name = std::string("box_db_.txt");
 
 
 
@@ -134,7 +137,7 @@ ifstream file(file_name);
 
 
 
-/*
+/* account the size of the file
 
 cout << "3:::::::::::::::" << endl;
 
@@ -264,7 +267,7 @@ bool count_cols_only_one = true;
 
 
 
-  KDL::Chain hand_finger[5];
+  KDL::Chain chains_hand_finger[5];
   KDL::Jacobian hand_jacob[5];
 
 
@@ -280,18 +283,18 @@ bool count_cols_only_one = true;
   end_chain_name[4] = "right_hand_little_distal_link";
 
 
-  hand_tree.getChain(root_name, end_chain_name[0], hand_finger[0]);      
-  hand_tree.getChain(root_name, end_chain_name[1], hand_finger[1]); 
-  hand_tree.getChain(root_name, end_chain_name[2], hand_finger[2]);
-  hand_tree.getChain(root_name, end_chain_name[3], hand_finger[3]);
-  hand_tree.getChain(root_name, end_chain_name[4], hand_finger[4]);
+  hand_tree.getChain(root_name, end_chain_name[0], chains_hand_finger[0]);      
+  hand_tree.getChain(root_name, end_chain_name[1], chains_hand_finger[1]); 
+  hand_tree.getChain(root_name, end_chain_name[2], chains_hand_finger[2]);
+  hand_tree.getChain(root_name, end_chain_name[3], chains_hand_finger[3]);
+  hand_tree.getChain(root_name, end_chain_name[4], chains_hand_finger[4]);
 
 
  
 
 
 
-///////////////////////////// 		END		///////////////////////////////////////////
+///////////////////////////// 		END		  ///////////////////////////////////////////
 	/////////////////////////////////////////////////////////////////////////
 
 		
@@ -300,7 +303,6 @@ bool count_cols_only_one = true;
 	/////////////////////////////////////////////////////////////////////////
 
 //			CALCULATE THE GRASP_MATRIX AND HAND_JACOBIAN FOR EACH ROW
-
 //	I supposed to have 19 joint variables and a single point of contact for each phalanx
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -308,20 +310,26 @@ bool count_cols_only_one = true;
 
 
 
-		unsigned int nj_0 = hand_finger[0].getNrOfJoints();   // 5
-  		unsigned int nj_1 = hand_finger[1].getNrOfJoints();   // 7
-  		unsigned int nj_2 = hand_finger[2].getNrOfJoints();  // 7
-  		unsigned int nj_3 = hand_finger[3].getNrOfJoints();    // 7
-  		unsigned int nj_4 = hand_finger[4].getNrOfJoints();  // 7 
+		unsigned int nj_0 = chains_hand_finger[0].getNrOfJoints();   // 5
+  		unsigned int nj_1 = chains_hand_finger[1].getNrOfJoints();   // 7
+  		unsigned int nj_2 = chains_hand_finger[2].getNrOfJoints();  // 7
+  		unsigned int nj_3 = chains_hand_finger[3].getNrOfJoints();    // 7
+  		unsigned int nj_4 = chains_hand_finger[4].getNrOfJoints();  // 7 
 
+
+cout << "nj_0 : " << nj_0 << endl;
+cout << "nj_1 : " << nj_1 << endl;
+cout << "nj_2 : " << nj_2 << endl;
+cout << "nj_3 : " << nj_3 << endl;
+cout << "nj_4 : " << nj_4 << endl;
 
 
 
   		KDL::JntArray q_thumb = JntArray(nj_0);    // thumb
     	KDL::JntArray q_index = JntArray(nj_1);    // forefinger
-    	KDL::JntArray q_middle = JntArray(nj_2);    // middlefinger
-    	KDL::JntArray q_ring = JntArray(nj_3);    // ringfinger
-    	KDL::JntArray q_little = JntArray(nj_4);    // littlefinger
+    	KDL::JntArray q_middle = JntArray(nj_2);   // middlefinger
+    	KDL::JntArray q_ring = JntArray(nj_3);     // ringfinger
+    	KDL::JntArray q_little = JntArray(nj_4);   // littlefinger
 
 
 
@@ -335,11 +343,11 @@ bool count_cols_only_one = true;
 
 
   // constructs the kdl solvers in non-realtime
-  jnt_to_jac_solver_0.reset(new KDL::ChainJntToJacSolver(hand_finger[0]));
-  jnt_to_jac_solver_1.reset(new KDL::ChainJntToJacSolver(hand_finger[1]));
-  jnt_to_jac_solver_2.reset(new KDL::ChainJntToJacSolver(hand_finger[2]));
-  jnt_to_jac_solver_3.reset(new KDL::ChainJntToJacSolver(hand_finger[3]));
-  jnt_to_jac_solver_4.reset(new KDL::ChainJntToJacSolver(hand_finger[4]));
+  jnt_to_jac_solver_0.reset(new KDL::ChainJntToJacSolver(chains_hand_finger[0]));
+  jnt_to_jac_solver_1.reset(new KDL::ChainJntToJacSolver(chains_hand_finger[1]));
+  jnt_to_jac_solver_2.reset(new KDL::ChainJntToJacSolver(chains_hand_finger[2]));
+  jnt_to_jac_solver_3.reset(new KDL::ChainJntToJacSolver(chains_hand_finger[3]));
+  jnt_to_jac_solver_4.reset(new KDL::ChainJntToJacSolver(chains_hand_finger[4]));
 
 
 
@@ -349,21 +357,21 @@ bool count_cols_only_one = true;
 
 
     	// resizes the joint state vectors in non-realtime
-  		q_thumb.resize(hand_finger[0].getNrOfJoints());
-  		q_index.resize(hand_finger[1].getNrOfJoints());
-  		q_middle.resize(hand_finger[2].getNrOfJoints());
-  		q_ring.resize(hand_finger[3].getNrOfJoints());
-  		q_little.resize(hand_finger[4].getNrOfJoints());
+  		q_thumb.resize(chains_hand_finger[0].getNrOfJoints());
+  		q_index.resize(chains_hand_finger[1].getNrOfJoints());
+  		q_middle.resize(chains_hand_finger[2].getNrOfJoints());
+  		q_ring.resize(chains_hand_finger[3].getNrOfJoints());
+  		q_little.resize(chains_hand_finger[4].getNrOfJoints());
 
-  		hand_jacob[0].resize(hand_finger[0].getNrOfJoints());
-  		hand_jacob[1].resize(hand_finger[1].getNrOfJoints());
-  		hand_jacob[2].resize(hand_finger[2].getNrOfJoints());
- 		hand_jacob[3].resize(hand_finger[3].getNrOfJoints());
-  		hand_jacob[4].resize(hand_finger[4].getNrOfJoints());
-
-
+  		hand_jacob[0].resize(chains_hand_finger[0].getNrOfJoints());
+  		hand_jacob[1].resize(chains_hand_finger[1].getNrOfJoints());
+  		hand_jacob[2].resize(chains_hand_finger[2].getNrOfJoints());
+ 		hand_jacob[3].resize(chains_hand_finger[3].getNrOfJoints());
+  		hand_jacob[4].resize(chains_hand_finger[4].getNrOfJoints());
 
 
+
+cout << " n_rows : " << n_rows << endl;
 
 
 
@@ -440,41 +448,32 @@ bool count_cols_only_one = true;
 
 
 
-
-
-
-
-		Eigen::MatrixXd Contacts(n_c,3); // I build a matrix of contact points for convenience only
+		Eigen::MatrixXd Contacts(n_c_max,3); // I build a matrix of contact points for convenience only
 										 // each row is a contact point
-										 // rows 0 and 1 for thumb
-										 // rows 2, 3, 4 for index
-										 // rows 5, 6, 7 for middle
-										 // rows 8, 9, 10 for ring
-										 // rows 11, 12, 13 for little
+										 // row 0, 1, 2 for thumb
+										 // row 3, 4, 5, 6 for index
+										 // row 7, 8, 9, 10 for middle
+										 // row 11, 12, 13, 14 for ring
+										 // row 15, 16, 17, 18 for little
+										 // n_c = 19 ( number of contact)
 
 
 
 		
-		int start = 29; // indice del primo valore utile nel database 
+		int start = 29; // the first value for the position of the joint variables
 		int gap_point_coordinate = 0;
 
-		for(int k = 0; k < n_c; k++)
+		for(int i = 0; i < n_c_max; i++)
 		{	
-			for(int h = 0; h < 3; h++ )
-				Contacts(k,h) = data_set(i, h + start + gap_point_coordinate);
+			for(int j = 0; j < 3; j++ )
+				Contacts(i,j) = data_set(r, start + gap_point_coordinate + j);  // check if it is correct
 
 			gap_point_coordinate += 3;
 
 		}
 
 
-		
-
-
-
-
-
-
+cout << "Contacts : " << Contacts << endl;
 
 
 
@@ -487,25 +486,27 @@ bool count_cols_only_one = true;
       	std::vector<Eigen::MatrixXd> Hand_Jacobian_ ;
 
 
+      	int k = 1;
 
-      	for(int i = 0 ; i < n_c ; i++) //calc the grasp_matrix and hand_jacobian for each contact point
+
+      	for(int i = 0 ; i < n_c_max ; i++) //calc the grasp_matrix and hand_jacobian for each contact point
       	{
       		
       		if(Contacts(i,0) != 9999) //9999 similar to NaN in dataset
       		{	
 
 
-      			Eigen::MatrixXd Grasp_Matrix(6,6);  // calculation of the already transposed
+      			Eigen::MatrixXd Grasp_Matrix(6,6);  
  				Eigen::MatrixXd Skew_Matrix(3,3);
   				Eigen::MatrixXd Rotation(3,3);
 
   				Rotation <<  MatrixXd::Identity(3,3); 
 
 
-
+  				
 
       			Skew_Matrix(0,0) = Skew_Matrix(1,1) = Skew_Matrix(2,2) = 0;
-            	Skew_Matrix(0,1) = - Contacts(i,2); // -rz
+            	Skew_Matrix(0,1) = - Contacts(i,2); // -rz    //check if the values ​​of the skew matrix are expressed in the correct reference system
             	Skew_Matrix(0,2) = Contacts(i,1);   // ry
             	Skew_Matrix(1,0) = Contacts(i,2);   // rz
             	Skew_Matrix(2,0) = - Contacts(i,1); // -ry
@@ -520,40 +521,50 @@ bool count_cols_only_one = true;
             	Grasp_Matrix.block<3,3>(3,0) = MatrixXd::Zero(3,3);
 
 
-            	cout << "Grasp_Matrix" << Grasp_Matrix << endl;
+            	cout << "Grasp_Matrix" << endl;
+            	cout << Grasp_Matrix << endl;
 
 
             	Grasp_Matrix_.push_back(Grasp_Matrix);
 
 
 
-            	jnt_to_jac_solver_0->JntToJac(q_thumb, hand_jacob[0],-1);
-      			jnt_to_jac_solver_1->JntToJac(q_index, hand_jacob[1],-1);
-      			jnt_to_jac_solver_2->JntToJac(q_middle, hand_jacob[2],-1);
-      			jnt_to_jac_solver_3->JntToJac(q_ring, hand_jacob[3],-1);
-      			jnt_to_jac_solver_4->JntToJac(q_little, hand_jacob[4],-1);
+            	jnt_to_jac_solver_0->JntToJac(q_thumb, hand_jacob[0], 0);
+      			jnt_to_jac_solver_1->JntToJac(q_index, hand_jacob[1], 0);
+      			jnt_to_jac_solver_2->JntToJac(q_middle, hand_jacob[2], 0);
+      			jnt_to_jac_solver_3->JntToJac(q_ring, hand_jacob[3], 0);
+      			jnt_to_jac_solver_4->JntToJac(q_little, hand_jacob[4], 0);
 
 
-      			cout << "Hand_Jacobian_ THUMB" << hand_jacob[0].data << endl;
-      			cout << "Hand_Jacobian_ INDEX" << hand_jacob[1].data << endl;
-      			cout << "Hand_Jacobian_ MIDDLE"<< hand_jacob[2].data << endl;
-      			cout << "Hand_Jacobian_ RING"  << hand_jacob[3].data << endl;
-      			cout << "Hand_Jacobian_ LITTLE"<< hand_jacob[4].data << endl;
+      			cout << "Hand_Jacobian_ THUMB" << endl;
+      			cout <<  hand_jacob[0].data << endl;
+      			cout << "Hand_Jacobian_ INDEX" << endl;
+      			cout <<  hand_jacob[1].data << endl;
+      			cout << "Hand_Jacobian_ MIDDLE"<< endl;
+      			cout <<  hand_jacob[2].data << endl;
+      			cout << "Hand_Jacobian_ RING"  << endl;
+      			cout <<  hand_jacob[3].data << endl;
+      			cout << "Hand_Jacobian_ LITTLE"<< endl;
+      			cout <<  hand_jacob[4].data << endl;
 
 
 
       			int which_finger = 0;
             	int which_falange = 0;
+            	
 
 
-            	if((0 <= i) && (i <= 2)){ which_finger = 0; which_falange = i;} // control flag which_falange there mey be some error
-            	if((3 <= i) && (i <= 6)){ which_finger = 1; which_falange = i-3;}
-            	if((7 <= i) && (i <= 10)){ which_finger = 2; which_falange = i-7;}
-            	if((11 <= i) && (i <= 14)){ which_finger = 3; which_falange = i-11;}
-            	if((15 <= i) && (i <= 18)){ which_finger = 4; which_falange = i-15;}
+            	if((0 <= i) && (i <= 2)){ which_finger = 0; which_falange = i+k; k++; if(i==2) k = 1;} // control flag which_falange there may be some error
+            	if((3 <= i) && (i <= 6)){ which_finger = 1; which_falange = i-3+k; k++; if(i==6) k = 1;}
+            	if((7 <= i) && (i <= 10)){ which_finger = 2; which_falange = i-7+k; k++; if(i==10) k = 1;}
+            	if((11 <= i) && (i <= 14)){ which_finger = 3; which_falange = i-11+k; k++; if(i==14) k = 1;}
+            	if((15 <= i) && (i <= 18)){ which_finger = 4; which_falange = i-15+k; k++; if(i==18) k = 1;}
 
 
 
+            	cout << "i : " << i << endl;
+            	cout << "which_finger : " << which_finger << endl;
+            	cout << "which_falange : " << which_falange << endl;
 
 
             switch(which_finger)
@@ -601,57 +612,65 @@ bool count_cols_only_one = true;
 
         	}// end if  contact
         	
+		
 
 
-
-        }// end for contact
+        }	// end for contact
 
 
         cout << " Dim of the vectors of the matrices grasp " << Grasp_Matrix_.size() << endl;
         cout << " Dim of the hand jacobian " << Hand_Jacobian_.size() << endl;
 
 
-        int n_c = Grasp_Matrix_.size();
+        int n_c_eff = Grasp_Matrix_.size();
 
 
         
-        Eigen::MatrixXd Hand_Jacobian_Contact(6*n_c,nq_hand-1);
-        Eigen::MatrixXd Grasp_Matrix_Contact(6,6*n_c);
+        Eigen::MatrixXd Hand_Jacobian_Contact(6*n_c_eff,nq_hand-1);
+        Eigen::MatrixXd Grasp_Matrix_Contact(6,6*n_c_eff);
 
 
 
         int s = 0; // dimension 
+        int s_ = 0;
 
 
-        for(int i = 0; i < n_c; i++)
+        for(int i = 0; i < n_c_eff; i++)
         {
         	Grasp_Matrix_Contact.block<6,6>(0,s) = Grasp_Matrix_[i];
         	
+        	cout << "i : " << i << endl;
 
-        	Hand_Jacobian_Contact.block<6,5>(i,0) = Hand_Jacobian_[i];
-        	Hand_Jacobian_Contact.block<6,7>(i,5) = Hand_Jacobian_[i+1];
-        	Hand_Jacobian_Contact.block<6,7>(i,12) = Hand_Jacobian_[i+2];
-        	Hand_Jacobian_Contact.block<6,7>(i,19) = Hand_Jacobian_[i+3];
-        	Hand_Jacobian_Contact.block<6,7>(i,26) = Hand_Jacobian_[i+4];
-
+        	Hand_Jacobian_Contact.block<6,5>(s,0) = Hand_Jacobian_[s_];
+        	Hand_Jacobian_Contact.block<6,7>(s,5) = Hand_Jacobian_[s_+1];
+        	Hand_Jacobian_Contact.block<6,7>(s,12) = Hand_Jacobian_[s_+2];
+        	Hand_Jacobian_Contact.block<6,7>(s,19) = Hand_Jacobian_[s_+3];
+        	Hand_Jacobian_Contact.block<6,7>(s,26) = Hand_Jacobian_[s_+4];
+			
+			s_+=5;
         	s+=6;
         }
 
-
-
-
-
-
+/*
+        file_output << "Grasp_Matrix_Contact : " << endl;
+        file_output <<  Grasp_Matrix_Contact << endl;
+        file_output << "__________________________________________________" << endl;
+        file_output << "Hand_Jacobian_Contact : " << endl;
+        file_output << Hand_Jacobian_Contact << endl;
+*/
 
         // GRASP JACOBIAN
 
 
         Eigen::MatrixXd GRASP_Jacobian(6,nq_hand-1);
+        Eigen::MatrixXd Grasp_Matrix_pseudo(Grasp_Matrix_Contact.rows(),Grasp_Matrix_Contact.cols()) ;
+        pseudo_inverse(Grasp_Matrix_Contact,Grasp_Matrix_pseudo);
 
-        Eigen::MatrixXd Grasp_Matrix_app = Grasp_Matrix_Contact.inverse();
+        GRASP_Jacobian = Grasp_Matrix_pseudo.transpose() * Hand_Jacobian_Contact;
 
-        GRASP_Jacobian = Grasp_Matrix_app.transpose() * Hand_Jacobian_Contact;
 
+        cout << "GRASP_Jacobian : " << endl;
+        cout << GRASP_Jacobian << endl;
 
 
         int quality_ = 9999;
@@ -664,11 +683,11 @@ bool count_cols_only_one = true;
         switch(quality_index) 
         {
         	case 0: // "minimum_singular_value_of_G"  Q = sigma_min(G)
-					{
+				{
         				JacobiSVD<MatrixXd> svd0(Grasp_Matrix_Contact, ComputeThinU | ComputeThinV);  
         				Singular = svd0.singularValues();
 						Quality.push_back(Singular[Singular.size()-1]);
-					}
+				}
         		break;
 
 
@@ -754,16 +773,25 @@ bool count_cols_only_one = true;
 				}
 				break;
 
-        }
+			
+
+        }//end switch
+
+        
+        cout << "Quality : " << endl;
 
 
+        for(int i = 0; i < Quality.size(); i++)
+			cout << Quality[i] << ' '; 
+
+		cout << endl;
 
 	} 
 
 
 
 
-  file.close();
+  
 
   cout << " YEAH ENJOY " << endl;
   cout << "   fine   " << endl;
