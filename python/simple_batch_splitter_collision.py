9666334012ac5f39f00d2448131ec335763a25f5
Antonio <liana.bertoni@gmail.com>
2017-04-05 17:43:46 +0200

    testing

---
+++ python/simple_batch_splitter_collision.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from mvbb.box_db import MVBBLoader
+import multiprocessing, subprocess
+from multiprocessing import Pool
+import sys
+
+from plugins import soft_hand
+
+def grasp_boxes(filename):
+    subprocess.call(['python', './grasp_boxes_batch_collision.py', filename])
+
+
+if __name__ == '__main__':
+    try:
+        import os.path
+        filename = os.path.splitext(sys.argv[1])[0]
+    except:
+        filename = 'box_db'
+
+    if not os.path.isfile(filename+'.csv'):
+        print "Error: file", filename, "doesn't exist"
+        exit()
+
+    try:
+        n_dofs = int(sys.argv[2])
+        n_l = int(sys.argv[3])
+    except:
+        n_dofs = soft_hand.numJoints
+        n_l = len(soft_hand.links_to_check)
+
+    # for SoftHand
+    box_db = MVBBLoader(filename, n_dofs, n_l)
+    filenames = box_db.split_db()
+
+    p = Pool(multiprocessing.cpu_count())
+    p.map(grasp_boxes, filenames)
+
+    box_db.join_results(filenames)
